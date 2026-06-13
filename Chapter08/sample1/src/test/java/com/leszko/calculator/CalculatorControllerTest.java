c2cd699db9a94e64621796c5f5a94e691dbe45f9
Govind <gp007s@gmail.com>
2026-06-13 15:51:00 -0400

    feat(@WebMvcTest): fixed the bean creation problem and add webMvcTest

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorControllerTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorControllerTest.java
@@ -1,22 +1,34 @@
 package com.leszko.calculator;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(CalculatorController.class)
 public class CalculatorControllerTest {
 
     @Autowired
-    private CalculatorController calculatorController;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private Calculator calculator;
 
     @Test
-    public void testSum() {
-        assertEquals("10", calculatorController.sum(7, 3));
+    public void testSum() throws Exception {
+        when(calculator.sum(2, 3)).thenReturn(5);
+        mockMvc.perform(get("/sum")
+                        .param("a", "2")
+                        .param("b", "3"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("5"));
     }
 }
